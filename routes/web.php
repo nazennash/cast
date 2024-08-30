1e98f925b22bfd9ced5a80da3dc4302858da67a1
Nash Oliver <92800845+nazennash@users.noreply.github.com>
2024-08-30 17:22:59 +0300

    Job-in-route

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    return view('jobs', 
-    [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Developer',
-                'salary' => '2000'
-            ],
-            [
-                'id'=> 2,
-                'title' => 'Programmer',
-                'salary' => '4000'
-            ]]
-        ]);   
-        
-});
-
-Route::get('/jobs/{id}', function ($id) {
-
-    $jobs = [
+class Job{
+    public static function all(): array
+    {
+        return [
             [
                 'id' => 1,
                 'title' => 'Developer',
@@ -48,11 +27,22 @@ Route::get('/jobs/{id}', function ($id) {
                 'title' => 'Programmer',
                 'salary' => '4000'
             ]
-        ];
+        ] ;
+    }
+}
 
-        $job = Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);  
 
-    return view('jobs', ['job' => $job]);
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs'=> Job::all()]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Illuminate\Support\Arr::first(Job::all(), fn($job) => $job['id'] == $id);  
+    return view('job', ['job' => $job]);
 });
 
 
